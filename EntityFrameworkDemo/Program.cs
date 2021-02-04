ebd5afd78626a8d1c08899f99c0391b0d3c87357
Ahmet <kucukonderahmet2009@hotmail.com>
2021-02-05 02:46:59 +0300

    EF Update

---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GetAll();
+            GetProductsByCategory(1);
+        }
+
+        private static void GetAll()
+        {
+            NorthwindContext northwindContext = new NorthwindContext();
+            foreach (var product in northwindContext.Products)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+        private static void GetProductsByCategory(int id)
+        {
+            NorthwindContext northwindContext = new NorthwindContext();
+
+            var result = northwindContext.Products.Where(p => p.CategoryId == id);
+
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
